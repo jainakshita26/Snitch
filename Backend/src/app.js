bee8677582d80f6dc711c51f4fba4abc1db0fe93
jainakshita26 <jainakshita9886@gmail.com>
2026-06-25 23:28:02 +0530

    google authentication

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -3,6 +3,9 @@ import cookieParser from 'cookie-parser'
 import morgan from 'morgan'
 import authRouter from './routes/auth.routes.js'
 import cors from 'cors'
+import passport from "passport"
+import {Strategy as GoogleStrategy} from "passport-google-oauth20"
+import { config } from './config/config.js'
 
 const app=express()
 
@@ -10,11 +13,20 @@ app.use(morgan("dev"))
 app.use(express.json())      //when frontend send json
 app.use(express.urlencoded({extended:true}))    //when frontend send form data
 app.use(cookieParser())
-// app.use(cors({
-//     origin:"http://localhost:5173",
-//     methods:["GET","POST","PUT","DELETE"],
-//     credentials:true
-// }))
+app.use(cors({
+    origin:"http://localhost:5173",
+    methods:["GET","POST","PUT","DELETE"],
+    credentials:true
+}))
+app.use(passport.initialize())
+
+passport.use(GoogleStrategy({
+    clientID:config.GOOGLE_CLIENT_ID,
+    clientSecret:config.GOOGLE_CLIENT_SECRET,
+    callbackURL:"/api/auth/google/callback"
+},(accessToken,refreshToken,profile,done)=>{
+    return done(null,profile)
+}))
 
 app.get('/',(req,res)=>{
     res.status(200).json({message:"Server is running"})
--- Backend/src/routes/auth.routes.js
+++ Backend/src/routes/auth.routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { validateLoginUser, validateRegisterUser } from "../validator/auth.validator.js";
 import { login, register } from "../controllers/auth.controller.js";
+import passport from "passport";
 
 const router=Router()
 
@@ -8,4 +9,5 @@ router.post('/register',validateRegisterUser,register)
 
 router.post('/login',validateLoginUser,login)
 
+router.get('/auth/google',passport.authenticate("google",{scope:["profile","email"]}))
 export default router;
