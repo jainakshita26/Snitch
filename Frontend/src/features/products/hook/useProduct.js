a115c56f08bc9928c088100cae734f35cb3cf877
jainakshita26 <jainakshita9886@gmail.com>
2026-07-02 22:58:24 +0530

    ProductDetail page added

--- Frontend/src/features/products/hook/useProduct.js
+++ Frontend/src/features/products/hook/useProduct.js
@@ -1,4 +1,4 @@
-import { createProduct, getSellerProduct, getAllProducts } from '../services/product.api.js'
+import { createProduct, getSellerProduct, getAllProducts, getProductById } from '../services/product.api.js'
 import { useDispatch } from 'react-redux'
 import { setSellerProducts, setProducts } from '../state/product.slice.js'     //have to store product in this action
 
@@ -17,11 +17,16 @@ export const useProduct = () => {
         return data.products
     }
 
-    async function handleGetAllProducts(){
-        const data=await getAllProducts()
+    async function handleGetAllProducts() {
+        const data = await getAllProducts()
         dispatch(setProducts(data.products))        //products decided form the controller 
 
     }
 
-    return { handleCreateProduct, handlegetSellerProduct ,handleGetAllProducts}
+    async function handleGetProductById(productId) {
+        const data = await getProductById(productId)
+        return product.data
+    }
+
+    return { handleCreateProduct, handlegetSellerProduct, handleGetAllProducts, handleGetProductById }
 }
